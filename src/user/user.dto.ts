526e9edd3172f7542eea26bea6222af2bb38c97f
yixiongyu <leoneyi@qq.com>
2021-12-04 00:06:05 +0800

    添加jwt生成方法

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,12 +1,22 @@
 import { ApiProperty } from "@nestjs/swagger";
 
+// 用户注册
 export class UserSignUpDto{
   @ApiProperty({type:String,required:true,description:'用户名'})
   username:string;
   @ApiProperty({type:String,required:true,description:'密码'})
   password:string;
 } 
+export class UserSignUpResDto{
+  @ApiProperty({type:Boolean,required:true,description:'结果，注册成功返回true'})
+  res:boolean;
+  @ApiProperty({type:String,required:false,description:'accessToken'})
+  accessToken?:string;
+  @ApiProperty({type:String,required:false,description:'freshToken'})
+  refreshToken?:string;
+} 
 
+// 用户登录
 export class UserSignInDto{
   @ApiProperty({type:String,required:true,description:'用户名'})
   username:string;
@@ -16,8 +26,8 @@ export class UserSignInDto{
 export class UserSignInResDto{
   @ApiProperty({type:Boolean,required:true,description:'结果，登陆成功返回true'})
   res:boolean;
-  @ApiProperty({type:String,required:true,description:'accessToken'})
-  accessToken:string;
-  @ApiProperty({type:String,required:true,description:'freshToken'})
-  refreshToken:string;
+  @ApiProperty({type:String,required:false,description:'accessToken'})
+  accessToken?:string;
+  @ApiProperty({type:String,required:false,description:'freshToken'})
+  refreshToken?:string;
 }

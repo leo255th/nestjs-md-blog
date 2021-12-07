a92f1cec701d9d3c7ce0a995e983c06d4077963e
yixiongyu <leoneyi@qq.com>
2021-12-07 11:03:55 +0800

    修改默认端口

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);  
   SwaggerModule.setup('api', app, document);
   app.enableCors({});
-  await app.listen(3002);
+  await app.listen(3000);
 }
 bootstrap();
